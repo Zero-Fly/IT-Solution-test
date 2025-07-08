d3dec101e882bf5e4efffd873cfb6013138ef10f
Zero Fly <anatolii_work@mail.ru>
2025-07-08 19:44:56 +0300

    add db model

--- quotes_test/quotes/models.py
+++ quotes_test/quotes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+class Quote(models.Model):
+    text = models.TextField(verbose_name="Quote text")
+    source = models.CharField(max_length=200, verbose_name="Source")
+    weight = models.PositiveIntegerField(verbose_name="Quote weight", default=1)
+    views = models.PositiveIntegerField(default=0, verbose_name="Views")
+    likes = models.PositiveIntegerField(default=0, verbose_name="Likes")
+    dislikes = models.PositiveIntegerField(default=0, verbose_name="Dislikes")
+
+    def clean(self):
+        # max quantity check
+        if Quote.objects.filter(source=self.source).count() >= 3:
+            if not self.pk or Quote.objects.get(pk=self.pk).source != self.source:
+                raise ValidationError(
+                    f"Reached maximum quantity"
+                )
+        # empty text    
+        if not self.text.strip():
+            raise ValidationError("Empty quote text")
+        #empty source
+        if not self.source.strip():
+            raise ValidationError("Empty quote source")
+        
+
+class Meta:
+    verbose_name = "Quote"
+    verbose_name_plural = "Quotes"
+    constraints = [
+        models.UniqueConstraint(
+            fields=['text', 'source'],
+            name='unique_quote_source'
+        )
+    ]
--- quotes_test/quotes/views.py
+++ quotes_test/quotes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import Quote
+import random
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    quotes = list(Quote.objects.all())
+    random_quote = random.choice(quotes) if quotes else None
+    return render(request, 'home.html', {'quote' : random_quote})
